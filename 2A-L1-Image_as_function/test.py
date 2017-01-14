daf1407648a48d1c733823c395a5ab71f04f3d46
mahavird <mahaviredx@gmail.com>
2017-01-14 10:50:39 +0530

    Opencv Essentials

--- 2A-L1-Image_as_function/test.py
+++ 2A-L1-Image_as_function/test.py
@@ -1,13 +1,27 @@
 import numpy as np
 import cv2
+#from matplotlib import *
+import matplotlib.pyplot as plt
 
 # Load an color image in grayscale
-img = cv2.imread('messi_5.jpg',0);
+img = cv2.imread('Fruits.jpg',0);
 
-cv2.imshow('image',img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+
 
 print img.shape;
 
 print img.dtype;
+
+
+color = ('b','g','r')
+for i,col in enumerate(color):
+	histr = cv2.calcHist([img],[i],None,[256],[0,256])
+	plt.plot(histr,color = col)
+	plt.xlim([0,256])
+
+plt.show()
+
+
+cv2.imshow('image',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
